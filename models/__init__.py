5893f5aa4559de1f7709d8cacda66392322065f2
Skomaiya <s.komaiya@alustudent.com>
2024-01-12 12:47:54 +0000

    task 5

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """__init__ magic method for models directory"""
-from model.engine.filestorage import file_storage
+from models.engine.filestorage import file_storage
 
 storage = file_storage()
 
