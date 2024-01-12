4f3a406446ddcd45ed77ca12a26dad8bebd83df9
Skomaiya <s.komaiya@alustudent.com>
2024-01-12 12:31:52 +0000

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,10 @@
 """
 Created the Module: BaseModel
 """
-
-import uuid
+import models
+from uuid import uuid4
 from datetime import datetime
-import models  # Assuming models is your storage module
+ 
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
@@ -27,20 +27,19 @@ class BaseModel:
             updated_at (datetime): The last update timestamp.
         """
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
         if kwargs:
             for key, value in kwargs.items():
                 if key == "__class__":
                     continue
                 elif key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value, time_format))
+                    self.__dict__[key] = datetime.strptime(value, tform)
                 else:
-                    setattr(self, key, value)
+                    self.__dict__[key] = value
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.today()
-            self.updated_at = datetime.today()
-
-        models.storage.new(self)
+            models.storage.new(self)
 
     def save(self):
         """
