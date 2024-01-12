c4ead8469c3f0953fedb4910bb11a67f658692a1
Willy kalisa <127384315+Kalisa21@users.noreply.github.com>
2024-01-12 13:29:35 +0000

    console

--- console.py
+++ console.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python
 """
-
+Airbnb Console: A simple console-based Airbnb clone.
 """
+
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
     """
-
+    HBNBCommand class: A class for the command-line interface.
     """
+
     prompt = "(hbnb) "
+
     def do_quit(self, arg):
         """
         Quit command to exit the program.
         """
         return True
-    
-    def help_quit(self. arg):
+
+    def help_quit(self, arg):
         """
-        
+        Display help information for the quit command.
         """
         print("Quit command to exit the program")
 
@@ -27,5 +30,8 @@ class HBNBCommand(cmd.Cmd):
         EOF (Ctrl+D) signal to exit the program.
         """
         return True
+
+
 if __name__ == '__main__':
-    HBNBCommand().cmdloop()    
+    # Instantiate HBNBCommand and start the command loop
+    HBNBCommand().cmdloop()
