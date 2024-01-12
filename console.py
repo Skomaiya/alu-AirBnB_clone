1cf12f49d2730d71e5432d0c66cc41e5adbbb5b9
Willy kalisa <127384315+Kalisa21@users.noreply.github.com>
2024-01-12 13:26:02 +0000

    console

--- console.py
+++ console.py
@@ -1,38 +1,31 @@
 #!/usr/bin/python
+"""
+
+"""
 import cmd
-import shlex
-from models.base_model import BaseModel
-from models import storage
+
+
 class HBNBCommand(cmd.Cmd):
+    """
+
+    """
     prompt = "(hbnb) "
-    valid_classes = ["BaseModel"]
-    def emptyline(self):
+    def do_quit(self, arg):
         """
-        Do nothing when an empty line is entered.
+        Quit command to exit the program.
         """
-        pass
+        return True
+    
+    def help_quit(self. arg):
+        """
+        
+        """
+        print("Quit command to exit the program")
 
     def do_EOF(self, arg):
         """
         EOF (Ctrl+D) signal to exit the program.
         """
         return True
-    def do_quit(self, arg):
-        """
-        Quit command to exit the program.
-        """
-        return True
-    def do_create(self, arg):
-        """
-        Create a new instance of BaseModel and save it to the JSON file.
-        Usage: create <class_name>
-        """
-        commands = shlex.split(arg)
-        if len(commands) == 0:
-            print("** class name missing **")
-        elif commands[0] not in self.valid_classes:
-            print("** class doesn't exist **")
-        else:
-            new_instance = BaseModel()
-            new_instance.save()
-            print(new_instance.id)
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()    
