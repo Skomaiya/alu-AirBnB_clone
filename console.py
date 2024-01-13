c3d8b25f2e81d85acd52e8b326ffbc67a1a9005f
Willy kalisa <127384315+Kalisa21@users.noreply.github.com>
2024-01-13 17:18:22 +0000

    task 7

--- console.py
+++ console.py
@@ -79,8 +79,53 @@ class HBNBCommand(cmd.Cmd):
             if key in objects:
                 print(objects[key])
             else:
-                print("** no instance found **")    
+                print("** no instance found **")   
 
+
+    def do_destroy(self, arg):
+        """
+        Delete an instance based on the class name and id.
+        Usage: destroy <class_name> <id>
+        """
+        commands = shlex.split(arg)
+
+        if len(commands) == 0:
+            print("** class name missing **")
+        elif commands[0] not in self.valid_classes:
+            print("** class doesn't exist **")
+        elif len(commands) < 2:
+            print("** instance id missing **")
+        else:
+            objects = storage.all()
+            key = "{}.{}".format(commands[0], commands[1])
+            if key in objects:
+                del objects[key]
+                storage.save()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, arg):
+        """
+        Print the string representation of all instances or a specific class.
+        Usage: <User>.all()
+                <User>.show()
+        """
+        objects = storage.all()
+
+        commands = shlex.split(arg)
+
+        if len(commands) == 0:
+            for key, value in objects.items():
+                print(str(value))
+        elif commands[0] not in self.valid_classes:
+            print("** class doesn't exist **")
+        else:
+            for key, value in objects.items():
+                if key.split('.')[0] == commands[0]:
+                    print(str(value))
+
+    
+    
 if __name__ == '__main__':
     # Instantiate HBNBCommand and start the command loop
     HBNBCommand().cmdloop()
