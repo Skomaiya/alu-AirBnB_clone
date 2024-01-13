5a955d28cde57d8f43556ccf73779861a58aac7f
Willy kalisa <127384315+Kalisa21@users.noreply.github.com>
2024-01-13 19:54:26 +0000

    task 8

--- models/base_model.py
+++ models/base_model.py
@@ -27,9 +27,9 @@ class BaseModel:
             updated_at (datetime): The last update timestamp.
         """
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
         if kwargs:
             for key, value in kwargs.items():
                 if key == "__class__":
